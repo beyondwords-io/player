aa747f3b19cc2592907eafdd540e2f609671845a
Chris Patuzzo <chris@patuzzo.co.uk>
2022-12-06 17:35:29 +0000

    Use ‘Library Mode’ when building the application
    https://vitejs.dev/guide/build.html#library-mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
+import { resolve } from "path"
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "BeyondWords",
+    },
+  },
 });
