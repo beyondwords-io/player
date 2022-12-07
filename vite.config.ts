fa8d154e54ba38f7a83c349f24b786d76e446547
Chris Patuzzo <chris@patuzzo.co.uk>
2022-12-07 11:29:37 +0000

    Shorten the dist/ filenames and ensure they end in .js
    I’m not sure whether all browsers will support script tags that use the
    .cjs file extension so make them all .js instead.

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "BeyondWords",
+      fileName: format => `${format}.js`,
     },
   },
   test: {
