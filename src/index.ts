d8bd2e1ba98556acc0daa4fd3cc56f4bd028faac
Chris Patuzzo <chris@patuzzo.co.uk>
2022-12-08 14:16:34 +0000

    Move the .instances() methods over to the new Player class

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import PlayerComponent from "./components/Player.svelte";
 import resolveTarget from "./helpers/resolveTarget";
 
 class Player extends PlayerComponent {
+  static #instances = [];
+
   constructor({ target, ...props }) {
     const { newTarget, showUserInterface } = resolveTarget(target);
 
@@ -15,6 +17,20 @@ class Player extends PlayerComponent {
         ...props,
       }
     });
+
+    Player.#instances.push(this);
+  }
+
+  static instances() {
+    return [...Player.#instances];
+  }
+
+  static destroyAll() {
+    Player.#instances.forEach(p => p.destroy());
+  }
+
+  destroy() {
+    Player.#instances = Player.#instances.filter(p => p !== this);
   }
 }
 
---
+++ test/index.test.ts
@@ -0,0 +1,43 @@
+import BeyondWords from "../src/index";
+
+describe("BeyondWords.Player", () => {
+  beforeEach(() => {
+    BeyondWords.Player.destroyAll();
+  });
+
+  describe(".instances", () => {
+    it("returns all player instances", () => {
+      const player1 = new BeyondWords.Player({});
+      expect(BeyondWords.Player.instances()).toEqual([player1]);
+
+      const player2 = new BeyondWords.Player({});
+      expect(BeyondWords.Player.instances()).toEqual([player1, player2]);
+    });
+  });
+
+  describe(".destroyAll", () => {
+    it("calls #destroy on all player instances", () => {
+      const player1 = new BeyondWords.Player({});
+      const player2 = new BeyondWords.Player({});
+
+      const spy1 = vi.spyOn(player1, "destroy");
+      const spy2 = vi.spyOn(player2, "destroy");
+
+      BeyondWords.Player.destroyAll();
+
+      expect(spy1).toHaveBeenCalledOnce();
+      expect(spy2).toHaveBeenCalledOnce();
+    });
+  });
+
+  describe("#destroy", () => {
+    it("removes the player from the static array", () => {
+      const player1 = new BeyondWords.Player({});
+      const player2 = new BeyondWords.Player({});
+
+      player1.destroy();
+
+      expect(BeyondWords.Player.instances()).toEqual([player2]);
+    });
+  });
+});
