d3d46947b22bffc243a93f2252ae2d8279479af3
Chris Patuzzo <chris@patuzzo.co.uk>
2022-12-12 15:25:08 +0000

    Extract a Duration component

---
+++ src/components/Duration.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let text;
+</script>
+
+<div>{text}</div>
+
+<style>
+  div {
+    font-size: 0.625rem;
+    font-weight: 300;
+  }
+</style>
--- src/components/ListenPrompt.svelte
+++ src/components/ListenPrompt.svelte
@@ -1,10 +1,13 @@
 <script>
+  import Duration from "./Duration.svelte";
+
   export let text = "Listen to this article";
+  export let duration = "4 min";
 </script>
 
 <div class="listen-prompt">
   <div class="text">{text}</div>
-  <div class="duration">4 min</div>
+  <Duration text={duration} />
 </div>
 
 <style>
@@ -18,10 +21,5 @@
   .text {
     font-size: 0.75rem;
     font-weight: 500;
-  }
-
-  .duration {
-    font-size: 0.625rem;
-    font-weight: 300;
   }
 </style>
