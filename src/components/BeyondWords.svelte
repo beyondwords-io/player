5ea3f9cde5ef10d17d0f7216dc0a17173ab2fb6a
Chris Patuzzo <chris@patuzzo.co.uk>
2022-12-13 13:41:05 +0000

    Add a BeyondWords component

---
+++ src/components/BeyondWords.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Logo from "./icons/Logo.svelte";
+
+  export let href;
+</script>
+
+<a href="https://beyondwords.io/" target="_blank" rel="noreferrer">
+  <Logo />
+</a>
+
+<style>
+  a {
+    display: flex;
+    margin-right: 0.5rem;
+  }
+</style>
