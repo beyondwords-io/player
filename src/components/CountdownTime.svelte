760937d021fcb8fc307901febc797ba5031808b8
Chris Patuzzo <chris@patuzzo.co.uk>
2022-12-13 16:13:34 +0000

    Add a CountdownTime component

---
+++ src/components/CountdownTime.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let text;
+  export let remaining;
+</script>
+
+<span>{text} • {remaining}</span>
+
+<style>
+  span {
+    font-size: 0.625rem;
+    font-weight: 300;
+  }
+</style>
