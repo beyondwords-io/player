f5261a348de28d9827acb1ce3c60003ea8fca31a
Chris Patuzzo <chris@patuzzo.co.uk>
2022-12-13 14:17:57 +0000

    Allow ProgressBar to style its related content
    On the designs, the progress bar always appears with some related
    content which is either the playback time or the time remaining of the
    advert. Therefore, I think it makes sense to move this content inside
    the ProgressBar and have two different style: “oneline” and “multiline”

--- src/components/ProgressBar.svelte
+++ src/components/ProgressBar.svelte
@@ -1,23 +1,51 @@
 <script>
   export let progress = 0;
+  export let style = "oneline";
 </script>
 
-<div class="progress-bar">
-  <div class="progress" style="width: {progress * 100}%">
+<div class="progress-bar {style}">
+  <slot></slot>
+
+  <div class="outer">
+    <div class="inner" style="width: {progress * 100}%"></div>
   </div>
 </div>
 
 <style>
   .progress-bar {
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+  }
+
+  .multiline {
+    flex-direction: column-reverse;
+    align-items: stretch;
+  }
+
+  .outer {
+    flex-grow: 1;
     height: 0.5rem;
     border-radius: 0.25rem;
     background: #d9d9d9;
-    margin: 0 0.5rem;
+    display: flex;
   }
 
-  .progress {
+  .inner {
     height: 0.5rem;
     border-radius: 0.25rem;
     background: #323232;
+  }
+
+  .oneline {
+    margin: 0 0.5rem;
+  }
+
+  .oneline .outer {
+    margin-left: 0.5rem;
+  }
+
+  .multiline .outer {
+    margin-bottom: 0.25rem;
   }
 </style>
