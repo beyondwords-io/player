466cfafc85b09bedc39ad6329d69b293ed6e1f72
Chris Patuzzo <chris@patuzzo.co.uk>
2022-12-22 15:50:17 +0000

    Add a NewTabButton component

---
+++ src/components/buttons/NewTabButton.svelte
@@ -0,0 +1,16 @@
+<script>
+  import OpenNewTab from "../svg_icons/OpenNewTab.svelte";
+
+  export let href;
+  export let scale = 1;
+</script>
+
+<a class="new-tab-button" href={href} target="_blank" rel="noreferrer" style="margin: 0 {0.8125 * scale}rem">
+  <OpenNewTab scale={scale} circular={false} />
+</a>
+
+<style>
+  .new-tab-button {
+    display: flex;
+  }
+</style>
