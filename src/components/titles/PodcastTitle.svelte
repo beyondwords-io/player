e788f037eceb2b7dfd72ff52d2b7b09e838b836c
Chris Patuzzo <chris@patuzzo.co.uk>
2022-12-23 10:28:54 +0000

    Add a ‘maxWidth’ parameter to PodcastTitle

--- src/components/titles/PodcastTitle.svelte
+++ src/components/titles/PodcastTitle.svelte
@@ -1,13 +1,17 @@
 <script>
   export let title;
   export let maxLines = 1;
+  export let maxWidth = undefined;
+
   export let bold = false;
   export let flex = "0 1 auto";
   export let scale = 1;
 
   $: fontSize = `${0.625 * scale}rem`;
   $: maxHeight = `${0.625 * scale * 1.2 * maxLines}`;
-  $: style = `--n: ${maxLines}; font-size: ${fontSize}; max-height: ${maxHeight}; flex: ${flex}`;
+  $: maxWidthCss = maxWidth ? `max-width: ${maxWidth}rem` : "";
+
+  $: style = `--n: ${maxLines}; font-size: ${fontSize}; max-height: ${maxHeight}; flex: ${flex}; ${maxWidthCss}`;
 </script>
 
 <div class="podcast-title" class:bold class:one-line={maxLines === 1} class:n-lines={maxLines > 1} {style}>
