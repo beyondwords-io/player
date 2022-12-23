6913a489687d0495e59ba54f81756f0cd9e03076
Chris Patuzzo <chris@patuzzo.co.uk>
2022-12-23 13:15:46 +0000

    Add a PlaylistButton component

---
+++ src/components/buttons/PlaylistButton.svelte
@@ -0,0 +1,16 @@
+<script>
+  import BurgerMenu from "../svg_icons/BurgerMenu.svelte";
+
+  export let scale = 1;
+</script>
+
+<div class="playlist-button">
+  <BurgerMenu scale={scale} />
+</div>
+
+<style>
+  .playlist-button {
+    display: flex;
+    cursor: pointer;
+  }
+</style>
