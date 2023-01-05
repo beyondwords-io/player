9f1ee995cd8c580040a048926a4564179989a2a3
Chris Patuzzo <chris@patuzzo.co.uk>
2023-01-05 14:57:39 +0000

    Add support for taking screenshots of widget (not fully working yet)

--- test/features/screenshotComparison.test.ts
+++ test/features/screenshotComparison.test.ts
@@ -13,20 +13,31 @@ const dimensions = {
     [{ title: "A reasonable length podcast title", image: podcastImage, duration: 30, externalUrl: "https://example.com" }],
     [{ title: `A ${"very ".repeat(50)} long title`, duration: 30 }, ...Array(10).fill({ title: "Another playlist item" })],
   ],
+  widgetStyle: ["none"],
+  widgetPosition: [null, "left", "center", "right"],
+  widgetWidth: ["auto", 0, "50%"],
 };
 
 test("screenshot comparison", async ({ page }) => {
   await page.goto("http://localhost:8000");
 
   for (const params of permutations(dimensions)) {
+    if (params.widgetPosition) { continue; } // TODO: remove
+
     if (params.playbackState === "stopped" && params.currentAdvert) { continue; }
     if (params.interfaceStyle === "small" && params.podcasts.length === 1) { continue; }
+
+    if (!params.widgetPosition && params.widgetWidth !== "auto") { continue; }
+    if (params.widgetPosition) { params.widgetStyle = params.interfaceStyle; }
 
     const bounds = await page.evaluate(async (params) => {
       const player = BeyondWords.Player.instances()[0];
       Object.entries(params).forEach(([k, v]) => player[k] = v);
 
-      const userInterface = player.target.querySelector(".user-interface")
+      const [scroll, selector] = params.widgetPosition ? [99999, ".fixed"] : [0, ":not(.fixed)"];
+      window.scrollTo(0, scroll);
+
+      const userInterface = player.target.querySelector(`.user-interface${selector}`);
       return userInterface.getBoundingClientRect();
     }, params);
 
@@ -35,6 +46,8 @@ test("screenshot comparison", async ({ page }) => {
       params.playbackState,
       params.currentAdvert ? "advert" : "content",
       params.podcasts.length > 1 ? "playlist" : "single",
+      params.widgetPosition ? params.widgetPosition : "static",
+      params.widgetWidth,
     ].join("-");
 
     await expect(page).toHaveScreenshot(`${screenshotName}.png`, { clip: bounds });
