dd00380cb78a21a316cf1914bb09c1624e5757e4
Chris Patuzzo <chris@patuzzo.co.uk>
2023-01-17 13:36:52 +0000

    Add keyboard controls for the ProgressCircle (play/pause/seek)

--- src/components/progress_bars/ProgressCircle.svelte
+++ src/components/progress_bars/ProgressCircle.svelte
@@ -1,9 +1,12 @@
 <script>
+  import newEvent from "../../helpers/newEvent";
+
   export let progress = 0;
   export let enabled = false;
   export let bold;
   export let scale = 1;
   export let color = "#323232";
+  export let onEvent = () => {};
 
   $: strokeWidth = scale * (bold ? 2.5 : 1.667);
 
@@ -13,10 +16,28 @@
   $: circumference = radius * 2 * Math.PI;
   $: clamped = Math.max(0, Math.min(1, progress));
   $: dashOffset = circumference * (1 - clamped);
+
+  export const handleKeyDown = (event) => {
+    let key;
+
+    if (event.key === "ArrowLeft")  { key = "Left"; }
+    if (event.key === "ArrowRight") { key = "Right"; }
+    if (event.key === " ")          { key = "Space"; }
+    if (event.key === "Enter")      { key = "Enter"; }
+
+    if (!key) { return; }
+    event.preventDefault();
+
+    onEvent(newEvent({
+      type: `Pressed${key}OnProgressBar`,
+      description: `The ${key.toLowerCase()} key was pressed while the progress bar was focussed.`,
+      initiatedBy: "user",
+    }));
+  };
 </script>
 
 {#if enabled}
-  <div class="progress-circle" style="width: {scale * 40}px; height: {scale * 40}px">
+  <button class="progress-circle" style="width: {scale * 40}px; height: {scale * 40}px" on:keydown={handleKeyDown}>
     <svg fill="none" width={size} height={size} xmlns="http://www.w3.org/2000/svg">
       <circle cx="50%"
               cy="50%"
@@ -28,7 +49,7 @@
     </svg>
 
     <slot></slot>
-  </div>
+  </button>
 {:else}
   <slot></slot>
 {/if}
@@ -39,6 +60,11 @@
     display: flex;
     align-items: center;
     justify-content: center;
+
+    background: none;
+    border: none;
+    margin: 0;
+    padding: 0;
   }
 
   .progress-circle svg {
