2f206c0224f4afca18164febd637dc25cab1f373
Chris Patuzzo <chris@patuzzo.co.uk>
2023-03-24 15:50:57 +0000

    Add a waitUntil helper

---
+++ src/helpers/waitUntil.ts
@@ -0,0 +1,9 @@
+const waitUntil = (conditionFn) => {
+  if (conditionFn()) { return; }
+
+  return new Promise(resolve => {
+    setInterval(() => conditionFn() && resolve(), 10)
+  });
+};
+
+export default waitUntil;
