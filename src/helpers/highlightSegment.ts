5db5dae8a22011bab146172bd9d05afc5fb206cf
Chris Patuzzo <chris@patuzzo.co.uk>
2023-04-03 14:26:48 +0100

    Unhighlight markers on the next highlightSegment call

--- src/helpers/highlightSegment.ts
+++ src/helpers/highlightSegment.ts
@@ -1,11 +1,15 @@
 const attribute = "data-beyondwords-marker";
-const markClass = "beyondwords-highlight";
+const markClasses = ["beyondwords-highlight", "bwp"];
 
 const highlightSegment = (segment, className) => {
-  // TODO: remove previous higlight
+  const markersToHighlight = new Set(findMarkers(segment));
+  const markersToUnhighlight = findAllMarkers().filter(m => !markersToHighlight.has(m));
 
-  const markers = findMarkers(segment);
+  highlight(markersToHighlight, className);
+  unhighlight(markersToUnhighlight, className);
+}
 
+const highlight = (markers, className) => {
   for (const marker of markers) {
     const mark = findInnerMark(marker) || createInnerMark(marker);
 
@@ -14,22 +18,49 @@ const highlightSegment = (segment, className) => {
   }
 };
 
+const unhighlight = (markers, className) => {
+  const classNames = new Set([...markClasses, className]);
+
+  for (const marker of markers) {
+    const mark = findInnerMark(marker);
+
+    if (hasOtherHighlights(mark, classNames)) {
+      mark.classList.remove(className);
+    } else if (mark) {
+      moveChildren({ fromElement: mark, toElement: marker });
+      mark.remove();
+    }
+  }
+};
+
+const findAllMarkers = () => (
+  [...document.querySelectorAll(`[${attribute}]`)]
+);
+
 const findMarkers = (segment) => {
   if (!segment?.marker) { return []; }
   return document.querySelectorAll(`[${attribute}="${segment.marker}"]`);
 };
 
 const findInnerMark = (element) => (
-  element.querySelector(`mark.${markClass}`)
+  element.querySelector(`mark.${markClasses.join(".")}`)
 );
 
 const createInnerMark = (element) => {
   const mark = document.createElement("mark");
 
-  mark.classList.add(markClass, "bwp");
+  mark.classList.add(...markClasses);
   element.appendChild(mark);
 
   return mark;
+};
+
+const hasOtherHighlights = (mark, classNames) => {
+  if (!mark) { return false; }
+
+  for (const className of mark.classList) {
+    if (!classNames.has(className)) { return true; }
+  }
 };
 
 const moveChildren = ({ fromElement, toElement }) => {
