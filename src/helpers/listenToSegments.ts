96b78762cd80e544228d0ec90689d8c3f794cdd2
Chris Patuzzo <chris@patuzzo.co.uk>
2023-04-04 15:32:01 +0100

    Prefer segments that match the player’s contentIndex

--- src/helpers/listenToSegments.ts
+++ src/helpers/listenToSegments.ts
@@ -33,23 +33,41 @@ const handleMouseUp = (event) => {
   if (!marker) { return; }
 
   for (const player of BeyondWords.Player.instances()) {
-    for (const [i, content] of (player.content || []).entries()) {
-      for (const [j, segment] of (content.segments || []).entries()) {
-        if (marker !== segment.marker) { continue; }
+    const { segment, contentIndex, segmentIndex } = chooseSegment(player, marker);
+    if (!segment) { continue; }
 
-        // TODO: if multiple players then prefer the one that's playing?
+    // TODO: if multiple players then prefer the one that's playing?
 
-        player.onEvent(newEvent({
-          type: "PressedArticleSegment",
-          description: "The user pressed on a segment in the article.",
-          initiatedBy: "user",
-          contentIndex: i,
-          segmentIndex: j,
-          segment,
-        }));
-      }
+    player.onEvent(newEvent({
+      type: "PressedArticleSegment",
+      description: "The user pressed on a segment in the article.",
+      initiatedBy: "user",
+      segment,
+      contentIndex,
+      segmentIndex,
+    }));
+  }
+};
+
+const chooseSegment = (player, marker) => {
+  let bestSoFar = {};
+  let bestContent = -Infinity;
+
+  for (const [i, content] of (player.content || []).entries()) {
+    for (const [j, segment] of (content.segments || []).entries()) {
+      if (marker !== segment.marker) { continue; }
+
+      // If the segment appears in the content more than once then choose the first
+      // segment that matches the player's contentIndex to avoid changing tracks.
+      const thisContent = player.contentIndex === i ? 1 : 0;
+      if (thisContent < bestContent) { continue; }
+
+      bestSoFar = { contentIndex: i, segmentIndex: j, segment };
+      bestContent = thisContent;
     }
   }
+
+  return bestSoFar;
 };
 
 listenToSegments();
