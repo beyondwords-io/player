31e2f79c5135208175ecc496ac41841012d3a229
Chris Patuzzo <chris@patuzzo.co.uk>
2023-04-04 14:56:53 +0100

    Simplify listenToSegments and don’t check target ancestors
    If the element that was clicked on is not the data-beyondwords-marker
    then don’t fire the event. Otherwise, anchor links are being intercepted
    by the event listener. This also simplifies the code to remove the
    callback which added unnecessary indirection.

--- src/helpers/listenToSegments.ts
+++ src/helpers/listenToSegments.ts
@@ -11,60 +11,42 @@ const handleMouseMove = (event) => {
   // TODO
 };
 
+let startX, startY;
+
 const handleMouseDown = (event) => {
   startX = event.pageX;
   startY = event.pageY;
-}
+};
 
 const handleMouseUp = (event) => {
-  if (mouseDragged(event)) { return; }
-  if (event.button !== 0) { return; }
-
-  const markersClicked = new Set(findClickedMarkers(event.target));
-  if (markersClicked.size === 0) { return; }
-
-  eachSegment(({ segment, player, contentIndex, segmentIndex }) => {
-    if (!markersClicked.has(segment.marker)) { return; }
-
-    // TODO: if multiple players then prefer the one that's playing?
-
-    player.onEvent(newEvent({
-      type: "PressedArticleSegment",
-      description: "The user pressed on a segment in the article.",
-      initiatedBy: "user",
-      contentIndex,
-      segmentIndex,
-      segment,
-    }));
-  });
-};
-
-let startX, startY;
-
-const mouseDragged = (event) => {
   const movedX = event.pageX - startX;
   const movedY = event.pageY - startY;
-
   const moved = Math.sqrt(movedX * movedX + movedY * movedY);
-  return moved > 5;
-};
 
-const findClickedMarkers = (target) => {
-  const markers = [];
+  const isDragged = moved > 5;
+  if (isDragged) { return; }
 
-  while (target && target.hasAttribute) {
-    markers.push(target.getAttribute(attribute));
-    target = target.parentNode;
-  }
+  const isLeftClick = event.button === 0;
+  if (!isLeftClick) { return; }
 
-  return markers.filter(m => m);
-};
+  const marker = event.target.getAttribute(attribute);
+  if (!marker) { return; }
 
-const eachSegment = (callback) => {
   for (const player of BeyondWords.Player.instances()) {
     for (const [i, content] of (player.content || []).entries()) {
       for (const [j, segment] of (content.segments || []).entries()) {
-        callback({ player, contentIndex: i, segmentIndex: j, segment });
+        if (marker !== segment.marker) { continue; }
+
+        // TODO: if multiple players then prefer the one that's playing?
+
+        player.onEvent(newEvent({
+          type: "PressedArticleSegment",
+          description: "The user pressed on a segment in the article.",
+          initiatedBy: "user",
+          contentIndex: i,
+          segmentIndex: j,
+          segment,
+        }));
       }
     }
   }
