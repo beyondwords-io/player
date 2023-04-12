5a5a6b93c766d43a4d551ed3da4e03f82043afee
Chris Patuzzo <chris@patuzzo.co.uk>
2023-04-12 17:33:00 +0100

    Reset styles for .beyondwords-segment-ui

--- src/components/StyleReset.svelte
+++ src/components/StyleReset.svelte
@@ -1,25 +1,33 @@
 <style>
-  :global(.beyondwords-player :not(svg *, iframe, .animating)) {
+  :global(.beyondwords-player :not(svg *, iframe, .animating)),
+  :global(.beyondwords-segment-ui :not(svg *, iframe, .animating)) {
     all: initial;
   }
 
-  :global(.beyondwords-player :focus-visible) {
+  :global(.beyondwords-player :focus-visible),
+  :global(.beyondwords-segment-ui :focus-visible) {
     outline: 1px dotted blue;
     outline: 5px auto -webkit-focus-ring-color;
   }
 
-  :global(.beyondwords-player svg) {
+  :global(.beyondwords-player svg),
+  :global(.beyondwords-segment-ui svg) {
     fill: transparent;
   }
 
-  :global(.beyondwords-player div) {
+  :global(.beyondwords-player div),
+  :global(.beyondwords-segment-ui div) {
     display: block;
   }
 
   :global(.beyondwords-player button),
-  :global(.beyondwords-player a),
   :global(.beyondwords-player button *),
-  :global(.beyondwords-player a *) {
+  :global(.beyondwords-player a),
+  :global(.beyondwords-player a *),
+  :global(.beyondwords-segment-ui button),
+  :global(.beyondwords-segment-ui button *),
+  :global(.beyondwords-segment-ui a),
+  :global(.beyondwords-segment-ui a *) {
     cursor: pointer;
   }
 
