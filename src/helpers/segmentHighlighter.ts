f8dc8824716aab1abbf3db584c5abe933380bc04
Chris Patuzzo <chris@patuzzo.co.uk>
2023-04-13 13:05:37 +0100

    Ensure .beyondwords-clickable is only added when segments are actually clickable
    Just because a segment is highlighted, it doesn’t mean it is also
    clickable and segments can also be clickable without being highlighted.

--- src/helpers/segmentHighlighter.ts
+++ src/helpers/segmentHighlighter.ts
@@ -7,23 +7,30 @@ const markerClasses = ["beyondwords-clickable", "bwp"];
 class SegmentHighlighter {
   static #mediator = new OwnershipMediator(this.#highlight, this.#unhighlight);
 
-  highlight(type, segment, highlightMode, modeWhenAuto, background) {
-    const mode = highlightMode === "auto" ? modeWhenAuto : highlightMode;
-    const enabled = mode === "all" || mode === "body" && segment?.section === "body";
+  highlight(type, segment, highlightMode, segmentPlayback, background) {
+    if (highlightMode === "auto") { highlightMode = segmentPlayback; }
+
+    const highlight = highlightMode === "all" || highlightMode === "body" && segment?.section === "body";
+    const clickable = segmentPlayback === "all" || segmentPlayback === "body" && segment?.section === "body";
+
+    if (!highlight) { background = null; }
 
     const previous = this[`prev${type}`];
-    const current = enabled ? segment?.marker : null;
+    const current = background || clickable ? segment?.marker : null;
 
-    if (current) { SegmentHighlighter.#mediator.addInterest(current, this, background); }
+    if (current) { SegmentHighlighter.#mediator.addInterest(current, this, background, clickable); }
     if (previous) { SegmentHighlighter.#mediator.removeInterest(previous, this); }
 
     this[`prev${type}`] = current;
   }
 
-  static #highlight(marker, background) {
+  static #highlight(marker, background, clickable) {
     const markerElements = document.querySelectorAll(`[${attribute}="${marker}"]`);
 
     for (const element of markerElements) {
+      if (clickable) { element.classList.add(...markerClasses); }
+      if (!background) { continue; } // Don't add marks if no background.
+
       const walker = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, false);
       let node;
 
@@ -42,15 +49,16 @@ class SegmentHighlighter {
         node.parentNode.insertBefore(mark, node);
         mark.appendChild(node);
       }
-
-      element.classList.add(...markerClasses);
     }
   }
 
-  static #unhighlight(marker) {
+  static #unhighlight(marker, background, clickable) {
     const markerElements = document.querySelectorAll(`[${attribute}="${marker}"]`);
 
     for (const element of markerElements) {
+      if (clickable) { element.classList.remove(...markerClasses); }
+      if (!background) { continue; } // There are no marks to remove.
+
       const markElements = element.querySelectorAll(`mark.${markClasses(marker)}`);
 
       for (const mark of markElements) {
@@ -59,8 +67,6 @@ class SegmentHighlighter {
           mark.remove();
         }
       }
-
-      element.classList.remove(...markerClasses);
     }
   }
 }
