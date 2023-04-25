0a20be0229875724ab9518fe106f05f1c8b0435e
Chris Patuzzo <chris@patuzzo.co.uk>
2023-04-25 15:26:39 +0100

    Implement placementsThatCanPlay for chooseIntroOutro

--- src/helpers/chooseIntroOutro.ts
+++ src/helpers/chooseIntroOutro.ts
@@ -1,15 +1,32 @@
 const chooseIntroOutro = ({ introsOutros, introsOutrosIndex, advertIndex, content, contentIndex, currentTime, atTheStart, atTheEnd, errored }) => {
   const currentIntroOutro = introsOutros[introsOutrosIndex];
 
-  if (currentIntroOutro) {
-    if (errored) { /* TODO */ }
-    if (atTheEnd) { return -1; } // Play the content.
+  x: if (currentIntroOutro) {
+    if (errored) { updateErroredIntrosOutros(currentIntroOutro); break x; } // Choose another intro/outro.
+    if (atTheEnd) { updatePlayedIntroOutroMedia(currentIntroOutro); return -1; } // Play the content.
     if (!atTheEnd) { return introsOutrosIndex; } // Keep playing the current intro/outro until it ends.
   }
 
   if (advertIndex !== -1) { return -1; } // Wait until the advert has finished.
 
+  const placements = placementsThatCanPlay({ content, contentIndex, currentTime, atTheStart, atTheEnd });
+
   return atTheStart ? 0 : -1; // TODO: placements
+};
+
+const updateErroredIntrosOutros = () => {}; // TODO
+const updatePlayedIntroOutroMedia = () => {}; // TODO
+
+const placementsThatCanPlay = ({ content, contentIndex, currentTime, atTheStart, atTheEnd }) => {
+  const eligiblePlacements = new Set();
+
+  const atTheStartOfAllContent = atTheStart && contentIndex === 0 && currentTime === 0;
+  const atTheEndOfAllContent = atTheEnd && contentIndex === content.length - 1;
+
+  if (atTheStartOfAllContent) { eligiblePlacements.add("pre-roll"); }
+  if (atTheEndOfAllContent) { eligiblePlacements.add("post-roll"); }
+
+  return eligiblePlacements;
 };
 
 export default chooseIntroOutro;
