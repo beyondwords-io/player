5c54329d39bcc89f11af26c9ca59362db48cead1
Chris Patuzzo <chris@patuzzo.co.uk>
2023-06-29 10:30:34 +0100

    Fall back to video sources if no audio sources are playable
    If the player is initialized with only video sources and a non-video
    playerStyle is visible, then we should still play the media.

--- src/helpers/orderedMediaSources.ts
+++ src/helpers/orderedMediaSources.ts
@@ -3,14 +3,14 @@ import elementIsVisible from "./elementIsVisible";
 const orderedMediaSources = (mediaObject, videoElement) => {
   if (!mediaObject) { return []; }
 
-  let sources;
-  if (elementIsVisible(videoElement)) {
-    sources = [...(mediaObject.video || []), ...(mediaObject.audio || [])];
-  } else {
-    sources = mediaObject.audio || [];
-  }
+  const audio = mediaObject.audio || [];
+  const video = mediaObject.video || [];
 
-  return [sources].flat().filter(s => s);
+  if (elementIsVisible(videoElement)) {
+    return [...video, ...audio];
+  } else {
+    return [...audio, ...video];
+  }
 };
 
 export default orderedMediaSources;
