92c942a33f79de46785f7c81981a2f2f6a2bc868
Chris Patuzzo <chris@patuzzo.co.uk>
2023-07-17 17:18:21 +0100

    Fix findLoadedMedia not working with #t=15 anchors

--- src/helpers/findLoadedMedia.ts
+++ src/helpers/findLoadedMedia.ts
@@ -1,10 +1,13 @@
-const findLoadedMedia = (sources, videoElement) => (
-  sources.find(source => {
-    const matchesUrl = source.url && source.url === videoElement?.currentSrc;
+const findLoadedMedia = (sources, videoElement) => {
+  const source = sources.find(source => {
+    const matchesUrl = source.url && videoElement?.currentSrc?.startsWith(source.url);
     const matchesHls = source.url?.endsWith(".m3u8") && videoElement?.currentSrc?.startsWith("blob:");
 
     return matchesUrl || matchesHls;
-  })
-);
+  });
+
+  if (!source) { console.warn("BeyondWords.Player: media was loaded but failed to set loadedMedia"); }
+  return source;
+};
 
 export default findLoadedMedia;
