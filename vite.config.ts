b66632818fe86969450714b53c8206f5ee554a2d
Chris Patuzzo <chris@patuzzo.co.uk>
2023-07-21 14:50:21 +0100

    Fix cache purging not working due to a missing version in umd.js
    https://github.com/beyondwords-io/speechkit-backend/blob/79bd81f25a90454403c703f7cd379cbbfe8c0e16/app/jobs/purge_proxy_cache_job.rb#L111

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from "path"
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { version } from "./package.json";
 import makeCssImportant from "./src/helpers/makeCssImportant";
 import prefixCssSelectors from "./src/helpers/prefixCssSelectors";
 
@@ -21,6 +22,9 @@ export default defineConfig({
     target: "es2015",
     rollupOptions: {
       external: [/hls.light.min.js/, /loadTheStyles.ts/],
+      output: {
+        banner: `/*! version:"${version}" */`,
+      },
     },
   },
   test: {
