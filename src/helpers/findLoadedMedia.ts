4c5e0a9c82bbc7c66cc944e40bee343964034dc6
Chris Patuzzo <chris@patuzzo.co.uk>
2023-08-11 11:10:56 +0100

    Make findLoadedMedia work with relative URLs
    The player-demos set the URL of the advert to ‘../deliveroo-advert.mp4’
    but the videoElement.currentSrc always contains the absolute URL:
    
    https://beyondwords-io.github.io/player-demo/deliveroo-advert.mp4

--- src/helpers/findLoadedMedia.ts
+++ src/helpers/findLoadedMedia.ts
@@ -1,6 +1,6 @@
 const findLoadedMedia = (sources, videoElement) => {
   const source = sources.find(source => {
-    const matchesUrl = source.url && videoElement?.currentSrc?.startsWith(source.url);
+    const matchesUrl = source.url && videoElement?.currentSrc === absoluteUrl(source.url);
     const matchesHls = source.url?.endsWith(".m3u8") && videoElement?.currentSrc?.startsWith("blob:");
 
     return matchesUrl || matchesHls;
@@ -8,6 +8,12 @@ const findLoadedMedia = (sources, videoElement) => {
 
   if (!source) { console.warn("BeyondWords.Player: media was loaded but failed to set loadedMedia"); }
   return source;
+};
+
+const absoluteUrl = (url) => {
+  const anchor = document.createElement("a");
+  anchor.href = url;
+  return anchor.href;
 };
 
 export default findLoadedMedia;
