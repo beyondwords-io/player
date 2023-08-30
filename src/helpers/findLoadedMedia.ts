5d2c4ecba1aa85738f82803d86b7b8edb1334e8a
nklhtv <nklhtv@gmail.com>
2023-08-30 18:19:31 +0300

    fix findLoadedMedia helper function
    ignore the time fragment in the video currentSrc

--- src/helpers/findLoadedMedia.ts
+++ src/helpers/findLoadedMedia.ts
@@ -1,13 +1,21 @@
 const findLoadedMedia = (sources, videoElement) => {
+  const videoSrc = srcWithoutHash(videoElement?.currentSrc);
   const source = sources.find(source => {
-    const matchesUrl = source.url && videoElement?.currentSrc === absoluteUrl(source.url);
-    const matchesHls = source.url?.endsWith(".m3u8") && videoElement?.currentSrc?.startsWith("blob:");
+    const matchesUrl = source.url && videoSrc === absoluteUrl(source.url);
+    const matchesHls = source.url?.endsWith(".m3u8") && videoSrc?.startsWith("blob:");
 
     return matchesUrl || matchesHls;
   });
 
   if (!source) { console.warn("BeyondWords.Player: media was loaded but failed to set loadedMedia"); }
   return source;
+};
+
+const srcWithoutHash = (src) => {
+  let url;
+  try { url = new URL(src); } catch (e) { return src; }
+  if (!url.hash) { return src; }
+  return src.slice(0, -url.hash.length);
 };
 
 const absoluteUrl = (url) => {
