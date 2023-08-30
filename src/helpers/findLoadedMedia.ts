7bc4842e20f7e04de65e74b08e99f0181d41ea8d
nklhtv <nklhtv@gmail.com>
2023-08-30 18:21:06 +0300

    handle optional src

--- src/helpers/findLoadedMedia.ts
+++ src/helpers/findLoadedMedia.ts
@@ -15,7 +15,7 @@ const srcWithoutHash = (src) => {
   let url;
   try { url = new URL(src); } catch (e) { return src; }
   if (!url.hash) { return src; }
-  return src.slice(0, -url.hash.length);
+  return src?.slice(0, -url.hash.length);
 };
 
 const absoluteUrl = (url) => {
