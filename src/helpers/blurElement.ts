cfc35796dc789b04f2ef82e6710d603154ec8fba
Chris Patuzzo <chris@patuzzo.co.uk>
2023-10-13 14:23:14 +0100

    Fix buttons being blurred when triggered by the NVDA screen reader
    https://linear.app/beyondwords/issue/S-3685/improve-player-accessibility
    
    The NVDA screen reader sends a mouseup event to click on buttons.
    Normally, mouseup events on buttons blur the element so that the video
    player hides the controls when the user moves their mouse away.
    
    To distinguish NVDA, use the technique here:
    
    https://stackoverflow.com/questions/52914854#answer-72588780

--- src/helpers/blurElement.ts
+++ src/helpers/blurElement.ts
@@ -1,3 +1,8 @@
-const blurElement = (event) => event.currentTarget.blur();
+const blurElement = (event) => {
+  const triggeredByNvdaScreenReader = event.detail === 0;
+  if (triggeredByNvdaScreenReader) { return; }
+
+  event.currentTarget.blur();
+};
 
 export default blurElement;
