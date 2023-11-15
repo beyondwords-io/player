384d8ca68fe81583f189f0980c62f32f77995142
Chris Patuzzo <chris@patuzzo.co.uk>
2023-11-15 17:23:34 +0000

    Add a script that precomputes GDPR daisybit strings

---
+++ src/helpers/writeDaisybitStrings.ts
@@ -0,0 +1,50 @@
+import { GVL, TCModel, TCString } from "@iabtechlabtcf/core";
+import fetch from "node-fetch";
+import fs from "fs";
+
+// This function writes a JavaScript file containing IAB TCF daisybit strings
+// that provide GDPR consent for either personalized or or non-personalized ads
+// across all known vendors from the vendor-list.json remote file.
+//
+// We can prepare these strings at build time because they won't change very
+// often and it avoids loading additional dependencies and making additional
+// requests as part of the player's loading process.
+//
+// This page has a handy encoder/decoder: https://iabtcf.com/#/encode
+
+const writeDaisybitStrings = async (jsOutputFilename) => {
+  const response = await fetch("https://vendor-list.consensu.org/v2/vendor-list.json");
+  const vendorList = await response.json();
+
+  const gvl = new GVL(vendorList);
+
+  const stacks = Object.values(gvl.stacks);
+  const vendors = Object.values(gvl.fullVendorList);
+
+  const nonPersonalizedStack = stacks.find(s => s.name === "Basic ads, and ad measurement");
+  const personalizedStack = stacks.find(s => s.name === "Personalised ads, and ad measurement");
+
+  const nonPersonalizedModel = new TCModel(gvl);
+  const personalizedModel = new TCModel(gvl);
+
+  // Allow all purposes based on whether personalized adverts are enabled in the player.
+  nonPersonalizedModel.purposeConsents.set(nonPersonalizedStack.purposes);
+  personalizedModel.purposeConsents.set(personalizedStack.purposes);
+
+  // Allow all available vendors to provide programmatic adverts to the player.
+  // The vendor list might change over time as new vendors are added.
+  for (const vendor of vendors) {
+    nonPersonalizedModel.vendorConsents.set(vendor.id);
+    personalizedModel.vendorConsents.set(vendor.id);
+  }
+
+  const nonPersonalizedDaisybit = TCString.encode(nonPersonalizedModel);
+  const personalizedDaisybit = TCString.encode(personalizedModel);
+
+  const objectToWrite = { nonPersonalizedDaisybit, personalizedDaisybit };
+  const jsonToWrite = JSON.stringify(objectToWrite, null, 2);
+
+  fs.writeFileSync(jsOutputFilename, jsonToWrite);
+};
+
+export default writeDaisybitStrings;
