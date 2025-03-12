93f50f248dcd573368ffb85453c40870cb4cf3dd
Chris Patuzzo <chris@patuzzo.co.uk>
2025-03-12 12:25:37 +0000

    Set highlightSections from the API

--- src/helpers/setPropsFromApi.ts
+++ src/helpers/setPropsFromApi.ts
@@ -100,7 +100,7 @@ const setProps = (player, data) => {
   set(player, "videoIconColor", videoColors.icon_color);
   set(player, "logoIconEnabled", data.settings.logo_icon_enabled);
   set(player, "logoImagePosition", data.video_settings.logo_image_position);
-  set(player, "highlightSections", data.settings.segment_playback_enabled ? "all" : "none");
+  set(player, "highlightSections", data.settings.segment_highlights_enabled ? "all" : "none");
   set(player, "clickableSections", data.settings.segment_playback_enabled ? "all" : "none");
   set(player, "segmentWidgetSections", "none");
   set(player, "analyticsConsent", analyticsConsent(data.settings));
