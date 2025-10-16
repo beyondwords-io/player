477f3e7884bcc3d41f7819081b58b6a3cf2fd6f9
nklhtv <nklhtv@gmail.com>
2025-10-16 11:27:23 +0300

    add react no-build demo

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Player } from "./Player";
+import newEvent from "./helpers/newEvent";
 
 if (typeof window !== "undefined") {
   window.BeyondWords ||= {};
   window.BeyondWords.Player ||= Player;
+  window.BeyondWords.newEvent ||= newEvent;
 }
 
 export * from "./uikit";
